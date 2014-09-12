e9f266200bec058baff14a8ee97c3dc9e38d2346
Sergey Rezvanov <sergey@rezvanov.info>
2014-09-12 18:26:10 +0600

    Do not use copyright_years Liquid tag

--- _plugins/copyright_years_tag.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  class CopyrightYearsTag < Liquid::Tag
-    def render(context)
-      "2011-#{Time.now.year}"
-    end
-  end
-end
-
-Liquid::Template.register_tag 'copyright_years', Jekyll::CopyrightYearsTag
